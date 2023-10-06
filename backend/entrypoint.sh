eb408d5b8ea60020221b062a5f1200edf31a8e90
deaxparadox <nitish136900kushwaha@gmail.com>
2023-10-06 10:07:38 +0530

    docker environment variables working

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,6 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic
-export PATH="$PATH:/home/normal/.local/bin"
-export PRODUCTION=1
-# which django-admin
-# echo "$PATH"
-python manage.py placeholder --populate --row 100
+python manage.py placeholder --populate --row 10
 echo
 python manage.py runserver 0.0.0.0:8000
